de487a799192286d196e7a7d6c8b0932e69d1406
ArsenZar <arsenzar@gmail.com>
2025-11-28 15:23:57 +0200

    learn 0.4v (typical patern take data from backend)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 // src/components/App.tsx
-
+import axios from "axios";
 import SearchForm from "./components/SearchForm";
+import { useState } from "react";
+
+interface Article {
+  objectID: string;
+  title: string;
+  url: string;
+}
+
+interface ArticlesHttpResponse {
+  hits: Article[];
+}
 
 export default function App() {
 
+  const [articles, setArticles] = useState<Article[]>([]);
+
   const handleSearch = async (topic: string) => {
     // Тут будемо виконувати HTTP-запит
-    console.log(topic);
+    const response = await axios.get<ArticlesHttpResponse>(
+      `https://hn.algolia.com/api/v1/search?query=${topic}`
+    );
+    setArticles(response.data.hits); 
   };
 
   return (
     <>
       <SearchForm onSubmit={handleSearch} />
+      {articles.length > 0 && (
+        <ul>
+          {articles.map(({ objectID, url, title }) => (
+            <li key={objectID}>
+              <a href={url} target="_blank">{title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
