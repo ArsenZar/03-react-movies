92a7c487eb668f5becbc40d5d2d65bcc5f689064
ArsenZar <arsenzar@gmail.com>
2025-11-28 17:54:29 +0200

    learn 0.6v (Loader)

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,23 @@ interface ArticlesHttpResponse {
 export default function App() {
 
   const [articles, setArticles] = useState<Article[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const handleSearch = async (topic: string) => {
     // Тут будемо виконувати HTTP-запит
+    setIsLoading(true);
     const response = await axios.get<ArticlesHttpResponse>(
       `https://hn.algolia.com/api/v1/search?query=${topic}`
     );
+    setIsLoading(false);
     setArticles(response.data.hits); 
   };
 
   return (
     <>
       <SearchForm onSubmit={handleSearch} />
+      {isLoading && <p>Loading data, please wait...</p>}
       {articles.length > 0 && <ArticleList items={articles} />}
     </>
   );
