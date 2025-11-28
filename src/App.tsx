6064e27beefb84e321be1ab57448d1a88e32340b
ArsenZar <arsenzar@gmail.com>
2025-11-28 23:25:17 +0200

    HW 0.9v (try useEffect, once without array second parametr)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(1);
   const [person, setPerson] = useState(null);
 
-  useEffect(() => { 
+  useEffect(() => {
     console.log('Effect ran!');
+    axios
+      .get('https://swapi.info/api/people/1')
+      .then((response) => setPerson(response.data));
+  }, []); // Порожній масив залежностей
 
-  axios
-    .get('https://swapi.info/api/people/1')
-    .then((response) => setPerson(response.data));
-  }, []);
-  console.log('App rendred!');
+  console.log('App rendered!');
+
   return (
     <>
+      <button onClick={() => setCount(count + 1)}>The count is {count}</button>
       <pre>{JSON.stringify(person, null, 2)}</pre>
     </>
   );
