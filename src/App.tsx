e309509443ea2579ce91b4f8029fd5570669d6b6
ArsenZar <arsenzar@gmail.com>
2025-11-28 20:03:22 +0200

    HW 0.8v (rerender bug)

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,18 @@
-// src/components/App.tsx
-import axios from "axios";
-import SearchForm from "./components/SearchForm";
-import { useState } from "react";
-import ArticleList from "./components/ArticleList";
-import type { Article } from "./types/article";
-
-interface ArticlesHttpResponse {
-  hits: Article[];
-}
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [person, setPerson] = useState(null);
 
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  // console.log('App rendred!');
 
-
-
-  const handleSearch = async (topic: string) => {
-    try {
-      // Тут будемо виконувати HTTP-запит
-      setIsLoading(true);
-      const response = await axios.get<ArticlesHttpResponse>(
-        `https://hn.algolia.com/api/v1/search?query=${topic}`
-      );
-      setIsLoading(false);
-      setArticles(response.data.hits);
-    } catch {
-      // 4. Встановлюємо стан isError в true
-      setIsError(true);
-    } finally {
-      // 5. Встановлюємо стан isLoading в false
-      // після будь якого результату запиту
-      setIsLoading(false);
-    }
-
-  };
+  axios
+    .get('https://swapi.info/api/people/1')
+    .then((response) => setPerson(response.data));
 
   return (
     <>
-      <SearchForm onSubmit={handleSearch} />
-      {isLoading && <p>Loading data, please wait...</p>}
-      {articles.length > 0 && <ArticleList items={articles} />}
+      <pre>{JSON.stringify(person, null, 2)}</pre>
     </>
   );
 }
