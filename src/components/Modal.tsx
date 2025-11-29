e15b4b5266a128ad470f666090413884dd3b680c
ArsenZar <arsenzar@gmail.com>
2025-11-29 13:15:55 +0200

    HW 0.19v (modal window add css)

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,14 @@
 // src/components/Modal.tsx
+
+import css from "./Modal.module.css";
+
 export default function Modal() {
     return (
-        <div>
-            <h2>Modal Title</h2>
-            <p>This is some content inside the modal.</p>
+        <div className={css.backdrop}>
+            <div className={css.modal}>
+                <h2>Modal Title</h2>
+                <p>This is some content inside the modal.</p>
+            </div>
         </div>
     );
 }
