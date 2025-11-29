b28e7b627c9cd8a98abd660980f3df20dbfca980
ArsenZar <arsenzar@gmail.com>
2025-11-29 13:28:54 +0200

    HW 0.21v (open/close)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-// src/components/App.tsx
-import Modal from "./components/Modal";
+import { useState } from 'react';
+import Modal from './components/Modal';
 
 export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div>
       <h1>Main content of the page</h1>
-      <Modal />
+      <button onClick={openModal}>Open modal</button>
+      {isModalOpen && <Modal onClose={closeModal} />}
     </div>
   );
 }
