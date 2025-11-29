5595ecb03615453575f70d7e1fdb8460517ac976
ArsenZar <arsenzar@gmail.com>
2025-11-29 13:35:52 +0200

    HW 0.22v (ready modal open/close + BGclick)

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,8 +8,20 @@ interface ModalProps {
 }
 
 export default function Modal({ onClose }: ModalProps) {
+
+    const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (event.target === event.currentTarget) {
+            onClose();
+        }
+    };
+
     return createPortal(
-        <div className={css.backdrop}>
+        <div
+            className={css.backdrop}
+            onClick={handleBackdropClick}
+            role="dialog"
+            aria-modal="true"
+        >
             <div className={css.modal}>
                 <button
                     className={css.closeButton}
