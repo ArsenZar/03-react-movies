119425ebb6a9ef180942f520e7ecea1a5907f524
ArsenZar <arsenzar@gmail.com>
2025-12-01 10:43:09 +0200

    ready to HW 0.1v

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 export default function App() {
-  const [clicks, setClicks] = useState(0);
-
-  // 1. Тільки один раз після монтування
-  useEffect(() => {
-    console.log("You can see me only once!");
-  }, []);
-
-  // 2. Кожного разу, коли змінюється clicks
-  useEffect(() => {
-    console.log("Clicks updated:", clicks);
-  }, [clicks]);
-
-  // 3. При кожному рендері (бо без залежностей)
-  useEffect(() => {
-    document.title = `You clicked ${clicks} times`;
-  });
+  
 
   return (
-    <button onClick={() => setClicks(clicks + 1)}>
-      You clicked {clicks} times
-    </button>
+    <></>
   );
 }
